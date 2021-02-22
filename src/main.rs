3969114910bb5584d608d2612a0d21ccbf193ce5
Daan Pape <daan@dptechnics.com>
2021-02-22 16:14:46 +0000

    Testing for softfloat added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-	println!("Hello World");
+	let a:f32 = 35.123;
+	let b:f32 = 3.14;
+
+	println!("Rust Hello World!");
+	println!("If no SIGILL happens, soft float works");
+	println!("The sum of {} and {} is {}", a, b, a + b);
 }
